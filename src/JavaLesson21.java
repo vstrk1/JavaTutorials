e9054d21098fa43559b509278155a3df5e55f85c
Vladislav <vladisen1244@gmail.com>
2015-08-23 18:11:02 +0300

    7 commit !!!!!

--- src/JavaLesson21.java
+++ src/JavaLesson21.java
@@ -2,141 +2,135 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class JavaLesson21 extends JFrame {
-    JButton button1;
-    JTextArea textArea1;
-    JTextField textField1;
-    int buttonClicked;
-    public static void main (String[] args){
+public class JavaLesson21 extends JFrame {          //создали класс , наследуемся от JFrame
+    JButton button1;                            //создали кнопку
+    JTextArea textArea1;                        //создали текст для ввода
+    JTextField textField1;                      //создали поле
+    int buttonClicked;                          //создали инту
+    public static void main (String[] args){    //создали метод который запускаект конструктор класса
         new JavaLesson21();
     }
-    public JavaLesson21(){
-        this.setSize(400, 400);
-        Toolkit tk = Toolkit.getDefaultToolkit();
-        Dimension dim = tk.getScreenSize();
+    public JavaLesson21(){                      //создали конструктор
+        this.setSize(400, 400);                 //установили размер
+        Toolkit tk = Toolkit.getDefaultToolkit();  //Короче создали обьект класа тулкит (библиотика для работы с окнами), устанавливаем набор инстументов по умалчанию
+        Dimension dim = tk.getScreenSize();     // Дименсион инкапсулирует  ширину и высоту компонента в определенный обьект. В данном случаем мы в обьект Dimension засунули
+        // метод , который возвращает нам размер экрана
 
-        int xPos = (dim.width / 2) - (this.getWidth() / 2 );
-        int yPos = (dim.height / 2)-(this.getHeight() / 2 );
+        int xPos = (dim.width / 2) - (this.getWidth() / 2 );    // создали инту , которая состоит из раности (ширины тул кита /2 - ширина Windows /2  )
+        int yPos = (dim.height / 2)-(this.getHeight() / 2 );    // то же самое с Y интой  , только повязано все на высоте
 
-        this.setLocation(xPos, yPos);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setTitle("My second frame");
-        JPanel thePanel = new JPanel();
-        ListenForButton lForButton = new ListenForButton();
-        button1 = new JButton();
-        button1.setText("Click Here");
-        button1.addActionListener(lForButton);
-        thePanel.add(button1);
-        textField1 = new JTextField("", 15);
-        textField1.setText("Type here");
-        ListenForKeys lForKeys = new ListenForKeys();
-        textField1.addKeyListener(lForKeys);
-        thePanel.add(textField1);
-        textArea1 = new JTextArea(15,20);
-        textArea1.setText("Tracking Events\n");
-        textArea1.setLineWrap(true);
-        textArea1.setWrapStyleWord(true);
-        JScrollPane scrollbar1 = new JScrollPane(textArea1 , JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-        thePanel.add(scrollbar1);
-        this.add(thePanel);
-        ListenForWindow  lForWindow = new ListenForWindow();
-        this.addWindowListener(lForWindow);
-        this.setVisible(true);
+        this.setLocation(xPos, yPos);   // установали местоположение окна согласно двум вышеустановленым интам
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // закрываем программу при закрытии окна
+        this.setTitle("My second frame"); // установили тайтл
+        JPanel thePanel = new JPanel(); // создали новую Jпанель
+        ListenForButton lForButton = new ListenForButton();    // создлали обьект типа лисенер (описан внизу) , иниализировали его
+        button1 = new JButton();        // инициализировали кнопку
+        button1.setText("Click Here"); //создали надпись на кнопке
+        button1.addActionListener(lForButton); // вызываем метод слушатель кнопки (выполняет екшен при нажатии) , подаем на него обьект lForButton , что находится выше
+        thePanel.add(button1);      // добавляем кнопку в thePanel
+        textField1 = new JTextField("", 15); // инициализируем новое поле , устанавливаем его содержание и длинну
+        textField1.setText("Type here"); // установили текст в поле
+        ListenForKeys lForKeys = new ListenForKeys(); // создали + инициализировали обьект (произвордит действие при нажатии клавиши)
+        textField1.addKeyListener(lForKeys); // добавили слушатель для поля ,засунули в ниго наш обьект , который выше на одну строчку
+        thePanel.add(textField1); // добавили это поле в thePanel
+        textArea1 = new JTextArea(15,20);  // создали поле для воода текста , установили размер
+        textArea1.setText("Tracking Events\n"); // установили текст в широкое поле
+        textArea1.setLineWrap(true);  // тру - текст переносится на новую строку . фолс - текст тянется дальше , можно листать его вправо
+        textArea1.setWrapStyleWord(true);   // тру - перенос по словам , фолс - по слогам/ вообще нет переноса ?
+        JScrollPane scrollbar1 = new JScrollPane(textArea1 , JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);//создали скрол
+        thePanel.add(scrollbar1); // "инициализировали скрол"
+        this.add(thePanel); // добавили панель в окно !!
+        ListenForWindow  lForWindow = new ListenForWindow();  // создали экшн при взаимодействии с окном
+        this.addWindowListener(lForWindow); // создали для окна лисенер , засунули в него обьект екшн , который выше
+        this.setVisible(true); // отображаем наше окно на мониторе
 
-        ListenForMouse lForMouse = new ListenForMouse();
-        thePanel.addMouseListener(lForMouse);
+        ListenForMouse lForMouse = new ListenForMouse(); // инициализировали новый обьект кторый чтото делает с мышкой
+        thePanel.addMouseListener(lForMouse); // добавили в панель лисенер , который принимает на себя екшн выше
 
     }
 
-
-
-
-
-        private class ListenForButton implements ActionListener{
-            public void actionPerformed(ActionEvent e){
-                if (e.getSource() == button1){
-                    buttonClicked++;
-                    textArea1.append("Button clicked " + buttonClicked + " times\n");
+    private class ListenForButton implements ActionListener{        // создали класс (действие при нажатии на кнопку) , реализует интерфейс
+            public void actionPerformed(ActionEvent e){         //реализация метода интерфейса . принимает на себе ивент (клик ? )
+                if (e.getSource() == button1){                  //ЕСЛИ (при ивенте равен кнопке ? )
+                    buttonClicked++;                    //увеличеваем инту на 1
+                    textArea1.append("Button clicked " + buttonClicked + " times\n");   // в большом текстовом поле пишем  сообщение + инта с кол-вом раз клика по кнопке
                 }
             }
 
-        }
+    }
 
-    private class ListenForKeys implements KeyListener{
-
-
-        @Override
-        public void keyTyped(KeyEvent e) {
-            textArea1.append("Key hit : " + e.getKeyChar() + "\n");
+    private class ListenForKeys implements KeyListener{     // создали прайват клас , реализует интерфейс слушателя кнопки
+        @Override // аннотация мметода
+        public void keyTyped(KeyEvent e) {      //метод от интерфейса , принимает на себя событие нажатия любой кнопки
+            textArea1.append("Key hit : " + e.getKeyChar() + "\n");  //выводи сообщение в поле + название нажатой кнопки
 
         }
 
-        @Override
+        @Override   // оверайд метод
         public void keyPressed(KeyEvent e) {
 
         }
 
-        @Override
+        @Override //
         public void keyReleased(KeyEvent e) {
-
-        }
+        } // закрыли клас
     }
 
-    private class ListenForWindow implements WindowListener{
+    private class ListenForWindow implements WindowListener{ // создали клас прайват для отображения екшена во время отображения окна // реализуем интерфейс
 
-        @Override
-        public void windowOpened(WindowEvent e) {
-            textArea1.append("Window is Created");
+        @Override// аннштация
+        public void windowOpened(WindowEvent e) { // метод от интерфейса , принимает на себя  ивент виндовса
+            textArea1.append("Window is Created"+ "\n"); // выдаем месседж о созданом окне
 
         }
 
-        @Override
+        @Override       // метод
         public void windowClosing(WindowEvent e) {
 
         }
 
-        @Override
+        @Override       //метод
         public void windowClosed(WindowEvent e) {
 
 
         }
 
-        @Override
+        @Override    //
         public void windowIconified(WindowEvent e) {
-            textArea1.append("Window is minimized");
+            textArea1.append("Window is minimized"+ "\n"); // выдаем меседж при уменьшении окна (у нас не работает потому что я запретил менять размер окна)
         }
 
         @Override
-        public void windowDeiconified(WindowEvent e) {
-            textArea1.append("Window in Normal State");
-
-        }
-
-        @Override
-        public void windowActivated(WindowEvent e) {
-            textArea1.append("Window is active");
+        public void windowDeiconified(WindowEvent e) { //метод "Де-минимизировано"
+            textArea1.append("Window in Normal State" + "\n"); // тоже не работает т.к. выключен ресайз
 
         }
 
         @Override
-        public void windowDeactivated(WindowEvent e) {
-            textArea1.append("Window is not active");
-        }
-    }
-
-    private class ListenForMouse implements MouseListener{
-
-        @Override
-        public void mouseClicked(MouseEvent e) {
-            textArea1.append("Mouse Panel pos : " + e.getX() + e.getY() + "\n");
-            textArea1.append("Mouse Screen pos : " + e.getXOnScreen() + e.getYOnScreen() + "\n");
-            textArea1.append("Mouse Button pos : " + e.getButton() + "\n");
-            textArea1.append("Mouse Clicks pos : " + e.getClickCount() + "\n");
+        public void windowActivated(WindowEvent e) {  // при активации окна показываем меседж
+            textArea1.append("Window is active " + "\n");
 
         }
 
         @Override
-        public void mousePressed(MouseEvent e) {
+        public void windowDeactivated(WindowEvent e) { // при деактицации окна тоже месседж
+            textArea1.append("Window is not active"+ "\n");
+        }
+    }           //покончили с методом
+
+    private class ListenForMouse implements MouseListener{      // создаем клас действия с мышкой , наследуемся от интерфейса
+
+        @Override               // реализуем метод интерфенйса
+        public void mouseClicked(MouseEvent e) { // метод , котрый принимает на себя клик мыши
+            textArea1.append("Mouse Panel pos : " + e.getX() + e.getY() + "\n");  // выводим в поле сообщение + координаты мыши
+            textArea1.append("Mouse Screen pos : " + e.getXOnScreen() + e.getYOnScreen() + "\n"); // координаты относительно экрана
+            textArea1.append("Mouse Button pos : " + e.getButton() + "\n"); //позиция кноки ?
+            textArea1.append("Mouse Clicks pos : " + e.getClickCount() + "\n"); // количество кликов
+
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) { // ПРОЧИЕ МЕТОДЫ КОТОРЫЕ НЕ РЕАЛИЗОВАНЫ В ДАННОМ КЛАССЕ
 
         }
 
--- src/JavaLesson22.java
+++ src/JavaLesson22.java
@@ -6,153 +6,157 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.NumberFormat;
 
-public class JavaLesson22 extends JFrame{
-    JButton button1;
-    JLabel label1, label2, label3;
-    JTextField textField1, textField2;
-    JCheckBox dollarSign, commaSeparator;
-    JRadioButton addNums, subtractNums, multNums, divideNums;
-    JSlider howManyTimes;
+public class JavaLesson22 extends JFrame{               // создали клас , унаследовались от Jrame а
+    JButton button1;  // сохдали кнопку
+    JLabel label1, label2, label3; // три надписи создали
+    JTextField textField1, textField2; // два поля
+    JCheckBox dollarSign, commaSeparator; // два чекбокса
+    JRadioButton addNums, subtractNums, multNums, divideNums; // 4 радиокнопки
+    JSlider howManyTimes; // один слайдер
 
-    double number1, number2, totalCalc;
-    public  static void main(String[] args){
+    double number1, number2, totalCalc;  //три переменные типа дабл
+    public  static void main(String[] args) { //создали запускающий метод , засунули в него конструктор класа
         new JavaLesson22();
     }
-    public  JavaLesson22(){
-        this.setSize(400, 400);
-        this.setLocationRelativeTo(null);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setTitle("My Third Frame !!");
-        JPanel thePanel = new JPanel();
 
-        button1 = new JButton("Calculate");
+    public  JavaLesson22(){         // создали конструктор
+        this.setSize(400, 400); // установили размер
+        this.setLocationRelativeTo(null); // позиционируем окно на центр экрана
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  // закрываем програму при выходе
+        this.setTitle("My Third Frame !!"); // установили тайтл
+        JPanel thePanel = new JPanel(); // создали + иницализировали новую панель
 
-        ListenForButton lForButton = new ListenForButton();
-        button1.addActionListener(lForButton);
+        button1 = new JButton("Calculate"); // иниализировали кнопку , дали ей имя
 
-        thePanel.add(button1);
-        label1 = new JLabel("Number 1");
-        thePanel.add(label1);
-        textField1 = new JTextField("", 5);
-        thePanel.add(textField1);
+        ListenForButton lForButton = new ListenForButton(); // создали экшн при клике на кнопку
+        button1.addActionListener(lForButton); // вызвали метод-слушалку , засунули в него екшн , который мы инициализировали выше
 
-        label2 = new JLabel("Number 2");
-        thePanel.add(label2);
-        textField2 = new JTextField("", 5);
-        thePanel.add(textField2);
+        thePanel.add(button1); // добавили кнопку в панель
+        label1 = new JLabel("Number 1"); // инициализировали лейбу , дали название
+        thePanel.add(label1); // добавили ее в панелоь
+        textField1 = new JTextField("", 5); // иниализировали поле , установили содержание + длинну поля
+        thePanel.add(textField1); // добавили поле в панель
 
-        dollarSign = new JCheckBox("Dollars");
-        commaSeparator = new JCheckBox("Commas");
+        label2 = new JLabel("Number 2"); // инициализировали вторую лейбу , дали ей имя
+        thePanel.add(label2); // добавили ее в панель
+        textField2 = new JTextField("", 5); // инициализировали филд , установили содержание и размер
+        thePanel.add(textField2); // добавили поле в панель
 
-        thePanel.add(dollarSign);
-        thePanel.add(commaSeparator,true);
+        dollarSign = new JCheckBox("Dollars"); // инициализировали чекбокс , дали ему имя
+        commaSeparator = new JCheckBox("Commas"); // и второму тоже
 
-        addNums = new JRadioButton("Add");
-        subtractNums = new JRadioButton("Subtract");
-        multNums = new JRadioButton("Multiply");
-        divideNums = new JRadioButton("Divide");
+        thePanel.add(dollarSign);           // добавляем в панель этот чекбокс
+        thePanel.add(commaSeparator,true);  // и этот
 
-        ButtonGroup operation = new ButtonGroup();
+        addNums = new JRadioButton("Add"); // инициализируем радиокнопку
+        subtractNums = new JRadioButton("Subtract"); // и эту
+        multNums = new JRadioButton("Multiply"); // и эту
+        divideNums = new JRadioButton("Divide"); // и эту
 
-        operation.add(addNums);
-        operation.add(subtractNums);
-        operation.add(multNums);
-        operation.add(divideNums);
+        ButtonGroup operation = new ButtonGroup(); // создали обьект типа баттонгруп (типо обьеденяем все кнопки в одну групу)
 
-        JPanel operPanel = new JPanel();
+        operation.add(addNums);         // добавляем в групу кнопку
+        operation.add(subtractNums);   //+
+        operation.add(multNums);      //+
+        operation.add(divideNums);   //+
 
-        Border operBorder = BorderFactory.createTitledBorder("Operation");
-        operPanel.setBorder(operBorder);
-        operPanel.add(addNums);
-        operPanel.add(subtractNums);
-        operPanel.add(multNums);
-        operPanel.add(divideNums);
+        JPanel operPanel = new JPanel(); //инициализируем новую  панель
 
-        addNums.setSelected(true);
+        Border operBorder = BorderFactory.createTitledBorder("Operation"); // создали обьект бордер , инициализируем его , лепим на бордер тайтл , пишем тайтл
+        operPanel.setBorder(operBorder); // утсанавливаем бордер в нашу новую панель
+        operPanel.add(addNums); // добавляем в новую панель радиоо кнопку
+        operPanel.add(subtractNums); //+
+        operPanel.add(multNums);    //+
+        operPanel.add(divideNums); //+
 
-        thePanel.add(operPanel);
+        addNums.setSelected(true); //устанавливаем данную кнопку в качестве изначального выбора
 
-        label3 = new JLabel("Perform How Many times");
+        thePanel.add(operPanel); // добавляем новую панель в нашу основую панель
 
-        thePanel.add(label3);
-        howManyTimes = new JSlider(0, 99, 1);
-        howManyTimes.setMinorTickSpacing(1);
-        howManyTimes.setMajorTickSpacing(10);
-        howManyTimes.setPaintTicks(true);
-        howManyTimes.setPaintLabels(true);
-        ListenForSlider lForSlider = new ListenForSlider();
-        howManyTimes.addChangeListener(lForSlider);
-        this.add(howManyTimes);
-        this.add(thePanel);
-        this.setVisible(true);
-        this.setResizable(false);
-        textField1.requestFocus();
-        thePanel.add(howManyTimes);
+        label3 = new JLabel("Perform How Many times"); //иницализировали новую лейбу
+
+        thePanel.add(label3); // добавили ее в панель
+        howManyTimes = new JSlider(0, 100, 1); // инициализировали слайдер , указали мин макс и значение одного деления
+        howManyTimes.setMinorTickSpacing(1);  // короче ставим количество маленьких полосочек между основными цифрами
+        howManyTimes.setMajorTickSpacing(10); // ставим количество больших черточек
+        howManyTimes.setPaintTicks(true);   //устанавливаем отображание тех самых полосочек
+        howManyTimes.setPaintLabels(true); //устанавливаем отображение десетков цифр
+        ListenForSlider lForSlider = new ListenForSlider(); // создали и инициализировали  экшн для слайдера
+        howManyTimes.addChangeListener(lForSlider); // прикрутили екшн к слайдеру
+        this.add(howManyTimes); // добавили слайдер к окну
+        this.add(thePanel); // добавили основную панель
+        this.setVisible(true); // включили отображение
+        this.setResizable(false); // нельзя менять масщтаб
+        textField1.requestFocus(); // при открытии окна первое поле активно
+        thePanel.add(howManyTimes); // добавили в панель слайдер
     }
 
-    private class ListenForButton implements ActionListener{
-        public void actionPerformed(ActionEvent e) {
-            if (e.getSource() == button1) {
-                try {
-                    number1 = Double.parseDouble(textField1.getText());
-                    number2 = Double.parseDouble(textField2.getText());
-                } catch (NumberFormatException excep) {
-                    JOptionPane.showMessageDialog(JavaLesson22.this, "Please Ennter the right info ", "Error", JOptionPane.ERROR_MESSAGE);
-                    System.exit(0);
+    private class ListenForButton implements ActionListener{  // создали прайват клас , екшн для кнопки
+        public void actionPerformed(ActionEvent e) { // реализуем метод интерфейса
+            if (e.getSource() == button1) { // если соурс равен кнопке
+                try {   //пробуем
+                    number1 = Double.parseDouble(textField1.getText());     //в первую дабл переменную засовываем значение , которое мы дали на пернвое поле, но перед этим оно преобразуется в тип дабл (с точкой короче)
+                    number2 = Double.parseDouble(textField2.getText()); // то же для второго числа
+                } catch (NumberFormatException excep) {     // ловим ексепшн
+                    JOptionPane.showMessageDialog(JavaLesson22.this, "Please Ennter the right info ", "Error", JOptionPane.ERROR_MESSAGE); //короче создаем поп-ап , вызываем метод для его отображенрия , подаем на него
+                    // компонент (в данном случае родительское окно) ,текст ошибки , тайт окна с ошибкой , тип сообщения
+                    System.exit(0); // выходим после сообщения
                 }
             }
-            if (addNums.isSelected()) {
-                totalCalc = addNumbers(number1, number2, howManyTimes.getValue());
+            if (addNums.isSelected()) {      //если выбранна данная радиокнопка
+                totalCalc = addNumbers(number1, number2, howManyTimes.getValue()); // вызываем метод сложения , суем в метод первое число , второе число , и значения слайдера , результат помещаем в переменную
 
-            } else if (subtractNums.isSelected()) {
-                totalCalc = subtractNumbers(number1, number2, howManyTimes.getValue());
-            } else if (multNums.isSelected()) {
-                totalCalc = multiplyNumbers(number1, number2, howManyTimes.getValue());
-            } else {totalCalc = divideNumbers(number1, number2, howManyTimes.getValue());
+            } else if (subtractNums.isSelected()) {         // тогда если (выбрана радиокнопка "вычитание")
+                totalCalc = subtractNumbers(number1, number2, howManyTimes.getValue()); //в totalcalc  засовываем функцию вычитания , на нее подаем первое второе число и значение слайдера
+            } else if (multNums.isSelected()) { // тогда если выбрана радиокнопка умножения
+                totalCalc = multiplyNumbers(number1, number2, howManyTimes.getValue()); // вызываем функцию умножения , опять  первое + второе число + значение слайдера , результат в переменную дабл
+            } else {totalCalc = divideNumbers(number1, number2, howManyTimes.getValue()); // тогда , (если выбрана радиокнопка "деление") вызываем фукнкцию деления , подаем на нее первое/второе чисол + значение слайдера
             }
 
-            if (dollarSign.isSelected()){
-                NumberFormat numFormat = NumberFormat.getCurrencyInstance();
-                JOptionPane.showMessageDialog(JavaLesson22.this, numFormat.format(totalCalc), "Solution", JOptionPane.INFORMATION_MESSAGE);
-            }else if(commaSeparator.isSelected()){
-                NumberFormat numFormat = NumberFormat.getNumberInstance();
-                JOptionPane.showMessageDialog(JavaLesson22.this, numFormat.format(totalCalc), "Solution", JOptionPane.INFORMATION_MESSAGE);
-            }else{ JOptionPane.showMessageDialog(JavaLesson22.this,totalCalc, "Solution", JOptionPane.INFORMATION_MESSAGE);
+            if (dollarSign.isSelected()){       // если выбран данный чекбокс
+                NumberFormat numFormat = NumberFormat.getCurrencyInstance(); // создаем  обьект , вызывам метод который устанавливает валюту в зависимости от текущей локали
+                JOptionPane.showMessageDialog(JavaLesson22.this, numFormat.format(totalCalc), "Solution", JOptionPane.INFORMATION_MESSAGE); // создаем поп-ап , вызывам метод для отображения ока , на него подаем родителя окна ,
+                // обьект, который возвращает нам число (результат вычислений) в определенной валюте , тайтл поп апа , тип окна информационный
+            }else if(commaSeparator.isSelected()){ //тогда если  выбран чек бокс "кома-сепаратор"
+                NumberFormat numFormat = NumberFormat.getNumberInstance(); // создаем обьект , вызываем метод , который возвращает  числовой формат (без точки) укапханной локали
+                JOptionPane.showMessageDialog(JavaLesson22.this, numFormat.format(totalCalc), "Solution", JOptionPane.INFORMATION_MESSAGE); // попап, отображаем его , в него засовываем родителя , формат отображения числа , на которое подаем результат
+                // тайтл окна , тип информационного окна
+            }else{ JOptionPane.showMessageDialog(JavaLesson22.this,totalCalc, "Solution", JOptionPane.INFORMATION_MESSAGE); //ИНАЧЕ просто показываем инфо окно
 
             }
         }
     }
-    private class ListenForSlider implements ChangeListener{
+    private class ListenForSlider implements ChangeListener{            //создали прайват клас (экшн для слайдера)
 
         @Override
-        public void stateChanged(ChangeEvent e) {
-            if(e.getSource() == howManyTimes){
-                label3.setText("Perform How Many Times? " + howManyTimes.getValue());
+        public void stateChanged(ChangeEvent e) {       // реализовываем метод
+            if(e.getSource() == howManyTimes){ // если метод равняется слайдеру
+                label3.setText("Perform How Many Times? " + howManyTimes.getValue()); // в лейбл ставим сообщение + значение слайдера
             }
 
         }
 
     }
-    public static double addNumbers(double number1, double number2, int howMany ){
-        double total = 0;
-        int i = 1;
-        while (i <= howMany){
-            total = total + (number1 + number2);
-            i++;
+    public static double addNumbers(double number1, double number2, int howMany ){ // создаем метод для сложение чисел (принимает на себя первое+второе + как часто)
+        double total = 0; // создали лабл , нуль
+        int i = 1; //создали инту с 1
+        while (i <= howMany){ // пока инта меньше или равно значениб слайдера
+            total = total + (number1 + number2); // в дабл пихаем сумму двух чисел, резульат в тотал суем
+            i++; // увеличиваем счетчик на 1
         }
-        return total;
+        return total; // метод возвращает результат
     }
 
-    public static double subtractNumbers(double number1, double number2, int howMany ){
-        double total = 0;
-        int i = 1;
-        while (i <= howMany){
-            total = total + (number1 - number2);
-            i++;
+    public static double subtractNumbers(double number1, double number2, int howMany ){ // создали такой же метод для вычитания
+        double total = 0; // создали нулевой дабл
+        int i = 1; // единицу
+        while (i <= howMany){   //пока..
+            total = total + (number1 - number2); // выполняем операцию с вычитанием , результат прибавляем к резульату
+            i++; // увеличиваем счечик на 1
         }
-        return total;
+        return total; // метод возвращает тотал
     }
-    public static double multiplyNumbers(double number1, double number2, int howMany ){
+    public static double multiplyNumbers(double number1, double number2, int howMany ){  //то же самое для умножения
         double total = 0;
         int i = 1;
         while (i <= howMany){
@@ -162,7 +166,7 @@ public class JavaLesson22 extends JFrame{
         return total;
     }
 
-    public static double divideNumbers(double number1, double number2, int howMany ){
+    public static double divideNumbers(double number1, double number2, int howMany ){ // и для деления
         double total = 0;
         int i = 1;
         while (i <= howMany){
