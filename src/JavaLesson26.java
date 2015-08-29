53f0c1d259661534cea853485cf51b10ff9b9647
Vladislav <vladisen1244@gmail.com>
2015-08-29 22:14:05 +0300

    9 commit

--- src/JavaLesson26.java
+++ src/JavaLesson26.java
@@ -5,60 +5,59 @@ import java.awt.event.ActionListener;
 import java.util.Calendar;
 import java.util.Date;
 
-public class JavaLesson26 extends JFrame {
-    JButton button1;
-    JSpinner spinner1, spinner2, spinner3, spinner4;
-    String outputString = " ";
+public class JavaLesson26 extends JFrame {  // создали класс
+    JButton button1;        //создали кнопку
+    JSpinner spinner1, spinner2, spinner3, spinner4;        // создали 4 спинера
+    String outputString = " ";       // создали стрингу
 
-    public static void main(String[] args) {
-        new JavaLesson26();
+    public static void main(String[] args) {            // запускающий метод
+        new JavaLesson26();                 // в него конструктор класса
 
     }
 
-    public JavaLesson26() {
-        this.setSize(400, 400);
-        this.setLocationRelativeTo(null);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setTitle("My Fifth Frame");
-        JPanel thePanel = new JPanel();
-        button1 = new JButton("Get Answer");
-        ListenForButton lForButton = new ListenForButton();
-        button1.addActionListener(lForButton);
-        thePanel.add(button1);
-        spinner1 = new JSpinner();
-        thePanel.add(spinner1);
-        spinner2 = new JSpinner(new SpinnerNumberModel(1, 1, 100, 1));
-        thePanel.add(spinner2);
-        String[] weekDays = {"Mon", "Tues", "Weds", "Thurs", "Fri"};
-        spinner3 = new JSpinner(new SpinnerListModel(weekDays));
-        Dimension d = spinner3.getPreferredSize();
-        d.width = 80;
-        spinner3.setPreferredSize(d);
-        thePanel.add(spinner3);
-        Date todaysDate = new Date();
-        spinner4 = new JSpinner(new SpinnerDateModel(todaysDate, null, null, Calendar.DAY_OF_MONTH));
-        JSpinner.DateEditor dateEditor = new JSpinner.DateEditor(spinner4, "dd/MM/yy");
-        spinner4.setEditor(dateEditor);
-        thePanel.add(spinner4);
-        //ListenForSpinner lForSpinner = new ListenForSpinner();
-        //spinner4.addChangeListener(lForSpinner);
-        this.add(thePanel);
-        this.setVisible(true);
+    public JavaLesson26() {         //создаем конструктор
+        this.setSize(400, 400);     // установили размер
+        this.setLocationRelativeTo(null);   // по середине окна
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);        // закрыть на выходе
+        this.setTitle("My Fifth Frame"); // тайтл
+        JPanel thePanel = new JPanel(); // новая панель
+        button1 = new JButton("Get Answer"); // иниц кнопку , дали название
+        ListenForButton lForButton = new ListenForButton(); // создали и иниц кастомный екшн
+        button1.addActionListener(lForButton);      //повесили на батон лисенер  , в него замунукли екшн
+        thePanel.add(button1);      //добавили кнопку в панель
+        spinner1 = new JSpinner(); // иниц нового спинера
+        thePanel.add(spinner1); //  добавили спинер в панель
+        spinner2 = new JSpinner(new SpinnerNumberModel(1, 1, 100, 1)); // иниц нового спинера
+        thePanel.add(spinner2);     //добавляем второй спинер в панель
+        String[] weekDays = {"Mon", "Tues", "Weds", "Thurs", "Fri"}; //создаем массив , пихаем туда стринги
+        spinner3 = new JSpinner(new SpinnerListModel(weekDays)); // инициализируем третий спинер , в него пихаем массив
+        Dimension d = spinner3.getPreferredSize(); //устанавливаем "предпочтительный" размер компонента , засовываем значение в Dimension
+        d.width = 80; //устанавливаем ширину спинера
+        spinner3.setPreferredSize(d); //применяем его к спинеру
+        thePanel.add(spinner3); // добавляем спинер в панель
+        Date todaysDate = new Date(); //создаем и инициализируем обьект Date
+        spinner4 = new JSpinner(new SpinnerDateModel(todaysDate, null, null, Calendar.DAY_OF_MONTH)); //  инициализируем третий спинер , устанавливаем параметры
+        // засовываем в него тип спинра , в тип с пинера дату  и тип календаря
+        JSpinner.DateEditor dateEditor = new JSpinner.DateEditor(spinner4, "dd/MM/yy"); // создаем и иницализируем едитор в него суем последний спинер и устанавливаем формат отображения даты
+        spinner4.setEditor(dateEditor); // устанавливаем формат в последний спине
+        thePanel.add(spinner4); //добавляем послений спинер в панель
+        this.add(thePanel); // добавляем панель в окно
+        this.setVisible(true); // устанавливаем видимое окно
     }
 
 
-    private class ListenForButton implements ActionListener {
+    private class ListenForButton implements ActionListener {  //инициализируем лисенер
 
         @Override
-        public void actionPerformed(ActionEvent e) {
-            if (e.getSource() == button1) {
-                outputString += "Spinner 1 Value " + spinner1.getValue() + "\n";
-                outputString += "Spinner 2 Value " + spinner2.getValue() + "\n";
-                outputString += "Spinner 3 Value " + spinner3.getValue() + "\n";
-                outputString += "Spinner 4 Value " + spinner4.getValue() + "\n";
-                JOptionPane.showMessageDialog(JavaLesson26.this, outputString, "Informatiion", JOptionPane.INFORMATION_MESSAGE);
+        public void actionPerformed(ActionEvent e) {   //
+            if (e.getSource() == button1) {  //когда кликаем на кнопку
+                outputString += "Spinner 1 Value " + spinner1.getValue() + "\n"; // в стрингу выводим значение первого спинера
+                outputString += "Spinner 2 Value " + spinner2.getValue() + "\n"; // второго
+                outputString += "Spinner 3 Value " + spinner3.getValue() + "\n"; //третьего
+                outputString += "Spinner 4 Value " + spinner4.getValue() + "\n"; // четверого
+                JOptionPane.showMessageDialog(JavaLesson26.this, outputString, "Informatiion", JOptionPane.INFORMATION_MESSAGE); //создаем поп ап с информацией , тайтлом
             }
-            outputString = " ";
+            outputString = " "; //обнуляем стринг
         }
 
 }
--- src/JavaLesson27.java
+++ src/JavaLesson27.java
@@ -10,90 +10,87 @@ import java.util.Enumeration;
 public class JavaLesson27 extends JFrame {
     JButton button1;
     String outputString = "";
-    JTree theTree;
-    DefaultMutableTreeNode document, work, games, emails;
-    DefaultMutableTreeNode fileSystem = new DefaultMutableTreeNode("C Drive");
+    JTree theTree;              // создаем обьект типа дерево
+    DefaultMutableTreeNode document, work, games, emails;       // СОЗДАЕМ ЧЕТЫРЕ ОСНОВНЫХ УЗЛА В ДЕРЕВЕ
+    DefaultMutableTreeNode fileSystem = new DefaultMutableTreeNode("C Drive"); // создаем и иницализируем еще один основной узел , даем ему имя
     public static void main (String[] args){
      new JavaLesson27();
-    }
-    public JavaLesson27(){
-        this.setSize(400, 400);
-        this.setLocationRelativeTo(null);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setTitle("My Sixth Frame");
-        JPanel thePanel = new JPanel();
-        button1 = new JButton("Get Answer");
+    } //
+    public JavaLesson27(){ // создали конструктор
+        this.setSize(400, 400); //
+        this.setLocationRelativeTo(null); //
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //
+        this.setTitle("My Sixth Frame"); //
+        JPanel thePanel = new JPanel(); //
+        button1 = new JButton("Get Answer"); //
 
-        ListenForButton lForButton = new ListenForButton();
-        button1.addActionListener(lForButton);
-        thePanel.add(button1);
+        ListenForButton lForButton = new ListenForButton();//
+        button1.addActionListener(lForButton); //
+        thePanel.add(button1); //
         theTree = new JTree(fileSystem);
-        theTree.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
-        theTree.setVisibleRowCount(8);
+        theTree.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION); // устанавливаем параметры дерева
+        theTree.setVisibleRowCount(8); // количество отображаемых строк в дереве
 
-        document = addFile("Docs", fileSystem);
-        addFile("Taxes.exl", document);
-        addFile("Story.tst", document);
+        document = addFile("Docs", fileSystem); // добавляем в основной узел узел поменьше
+        addFile("Taxes.exl", document); // в него добавляем типо "файлы :) "
+        addFile("Story.tst", document); // еще один
 
-        emails = addFile("Emails", document);
-        addFile("Shedul.txt", document);
-        addFile("CallBob.txt", emails);
+        emails = addFile("Emails", document); // в папку документов добавдяем еще одну папку с емейлами :)
+        addFile("Shedul.txt", document); // типо файлы
+        addFile("CallBob.txt", emails); // еще один
 
-        work = addFile("Work Applications", fileSystem);
-        addFile("Spreadsheet.exe", work);
+        work = addFile("Work Applications", fileSystem); // основное дерево суем  папку ворк
+        addFile("Spreadsheet.exe", work); // в папку  суем два файла
         addFile("Worldprocessor.exe", work);
 
-        games = addFile("Games", fileSystem);
-        addFile("Spaceinvaders.exe", games);
+        games = addFile("Games", fileSystem);  // добавляем в основной нод папку с лудоманией
+        addFile("Spaceinvaders.exe", games); // добавили файлы
         addFile("PacMan.exe", games);
 
-        JScrollPane scrollBox = new JScrollPane(theTree);
-        Dimension d = scrollBox.getPreferredSize();
-        d.width = 200;
-        scrollBox.setPreferredSize(d);
-        thePanel.add(scrollBox);
+        JScrollPane scrollBox = new JScrollPane(theTree); // создали и иницализировали скрол
+        Dimension d = scrollBox.getPreferredSize(); // создали переменую
+        d.width = 200; // установили ширину
+        scrollBox.setPreferredSize(d); // установили  ширину скрола
+        thePanel.add(scrollBox);  // добавили скрол в панель
 
 
 
-        this.add(thePanel);
-        this.setVisible(true);
+        this.add(thePanel); // добавили панель в окно
+        this.setVisible(true); // установили отображение окна
     }
-    private class ListenForButton implements ActionListener{
-
+    private class ListenForButton implements ActionListener{            // лисенер для кнопки
         @Override
-        public void actionPerformed(ActionEvent e) {
-            if(e.getSource() == button1){
-                Object treeObject = theTree.getLastSelectedPathComponent();
-                DefaultMutableTreeNode theFile = (DefaultMutableTreeNode) treeObject;
-                String treeNode = (String) theFile.getUserObject();
-                outputString += "The Selected Node : " + treeNode+ "\n";
-                outputString += "Nmbr of Children: " + theFile.getChildCount()+ "\n";
-                outputString += "Nmbr of Siblings: " + theFile.getSiblingCount()+ "\n";
-                outputString += "Parent  : " + theFile.getParent() + "\n";
-                outputString += "Next node  : " + theFile.getNextNode() + "\n";
-                outputString += "Previous Node : " + theFile.getPreviousNode() + "\n";
-                outputString += "\nChildren of Node\n" ;
-                for (Enumeration enumValue = theFile.children(); enumValue.hasMoreElements();){
-                    outputString += enumValue.nextElement() + "\n";
+        public void actionPerformed(ActionEvent e) {                 //
+            if(e.getSource() == button1){                           // если кликаем на кнопку
+                Object treeObject = theTree.getLastSelectedPathComponent();     //создали обькет , засунули в него последний выбранный компонент
+                DefaultMutableTreeNode theFile = (DefaultMutableTreeNode) treeObject;       // создали и иницализировали обьект
+                String treeNode = (String) theFile.getUserObject();         //
+                outputString += "The Selected Node : " + treeNode+ "\n"; // дали в строку
+                outputString += "Nmbr of Children: " + theFile.getChildCount()+ "\n";   //кол-во детей
+                outputString += "Nmbr of Siblings: " + theFile.getSiblingCount()+ "\n";// родой элемент
+                outputString += "Parent  : " + theFile.getParent() + "\n";            // указываем родителей
+                outputString += "Next node  : " + theFile.getNextNode() + "\n";      //выводи
+                outputString += "Previous Node : " + theFile.getPreviousNode() + "\n";  // указываем
+                outputString += "\nChildren of Node\n" ;                               // добавляем строку
+                for (Enumeration enumValue = theFile.children(); enumValue.hasMoreElements();){     //крутим цикл столько сколько у нас детей
+                    outputString += enumValue.nextElement() + "\n"; // добавляем следующий элемент
                 }
-                outputString += "\nPath From Root\n";
-                TreeNode[] pathNodes = theFile.getPath();
-                for (TreeNode indivNodes : pathNodes){
-                    outputString += indivNodes + "\n";
+                outputString += "\nPath From Root\n";         // еще строка
+                TreeNode[] pathNodes = theFile.getPath();    // получаем путь , пишем его в массив
+                for (TreeNode indivNodes : pathNodes){      // крутим цикл столько сколько у нас путей в массиве
+                    outputString += indivNodes + "\n";     //
                 }
-
-
-                JOptionPane.showMessageDialog(JavaLesson27.this, outputString, "Catalog", JOptionPane.INFORMATION_MESSAGE);
+                JOptionPane.showMessageDialog(JavaLesson27.this, outputString, "Catalog", JOptionPane.INFORMATION_MESSAGE);//создали поп ап , подали стрингу , дали тайтл , опредили тип пап-ап
                 outputString = "";
             }
         }
     }
 
-    private DefaultMutableTreeNode addFile(String fileName, DefaultMutableTreeNode parenFolder){
-        DefaultMutableTreeNode newFile = new DefaultMutableTreeNode(fileName);
-        parenFolder.add(newFile);
+    private DefaultMutableTreeNode addFile(String fileName, DefaultMutableTreeNode parenFolder){ // создаем метод , принимает на себя имя файла , и обьек
+        DefaultMutableTreeNode newFile = new DefaultMutableTreeNode(fileName);  //  инициализируем новый файл
+        parenFolder.add(newFile); // добавляем в папку новый файл
 
-        return newFile;
+        return newFile; // возвращаем новый файл 
     }
 
 
